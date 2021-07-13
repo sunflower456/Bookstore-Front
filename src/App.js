9bcb4021b82484db73f61518394a309489401b80
sunflower <ghkdalki456@naver.com>
2021-07-13 09:43:24 +0900

    add node modules

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import TEST from './features/counter/TEST';
+
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <TEST />
+      </div>
+    );
+  }
+}
+
+export default App;
