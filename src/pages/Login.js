5880dc9671c5b461ac0ef19ffc93f55ace17f45e
gentledot <gentledot.wp@gmail.com>
2021-07-26 19:28:32 +0900

    feat : login page UI 제작 (2)

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,61 +1,76 @@
 import {Link as RouterLink} from 'react-router-dom';
 // material
 import {styled} from '@material-ui/core/styles';
-import {Card, Container, Link, Typography} from '@material-ui/core';
-// layouts
-import AuthLayout from '../layouts/AuthLayout';
+import {Card, Container, Grid, Link, Typography} from '@material-ui/core';
 // components
 import Page from '../components/Page';
 import {default as LoginPage} from '../components/login/Login'
+import {MHidden} from '../components/@material-extend';
+// images
+import bookImage from '../static/images/book_sales.jpg';
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled(Page)(({ theme }) => ({
-  [theme.breakpoints.up('md')]: {
-    display: 'flex'
-  }
+const RootStyle = styled(Page)(({theme}) => ({
+    [theme.breakpoints.up('md')]: {
+        display: 'flex',
+        marginTop: theme.spacing(10),
+    }
 }));
 
-const SectionStyle = styled(Card)(({ theme }) => ({
-  width: '100%',
-  maxWidth: 464,
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  margin: theme.spacing(2, 0, 2, 2)
+const SectionStyle = styled(Card)(({theme}) => ({
+    width: '100%',
+    maxWidth: 464,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    margin: theme.spacing(2, 0, 2, 2)
 }));
 
-const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
-  margin: 'auto',
-  display: 'flex',
-  minHeight: '100vh',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  padding: theme.spacing(12, 0)
+const ContentStyle = styled('div')(({theme}) => ({
+    maxWidth: 480,
+    margin: 'auto',
+    display: 'flex',
+    minHeight: '100vh',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    padding: theme.spacing(12, 0)
 }));
 
 // ----------------------------------------------------------------------
 
 export default function Login() {
-  return (
-    <RootStyle title="Login | Minimal-UI">
-      <AuthLayout>
-        Don’t have an account? &nbsp;
-        <Link underline="none" variant="subtitle2" component={RouterLink} to="/register">
-          Get started
-        </Link>
-      </AuthLayout>
-
-        <SectionStyle>
-          <Typography variant="h3" sx={{ px: 5, mt: 10, mb: 5 }}>
-            Hi, Welcome Back
-          </Typography>
-        </SectionStyle>
-
-      <Container maxWidth="sm">
-        <LoginPage/>
-      </Container>
-    </RootStyle>
-  );
+    return (
+        <RootStyle title="Login | Herb Book Store">
+            <Grid container spacing={2}>
+                <Grid item md={10}>
+                    &nbsp;
+                </Grid>
+                <Grid item md={2} xs={12} sx={{textAlign: 'right', pr: '1em'}}>
+                    회원이 아니시라면?
+                    <Link underline="none" variant="subtitle2" component={RouterLink} to="/register">
+                        &nbsp; 회원가입
+                    </Link>
+                </Grid>
+                <Grid item md={3} xs={0}>
+                    <MHidden width="mdDown">
+                        <SectionStyle>
+                            <Typography variant="h3" sx={{px: 5, mt: 10, mb: 5}}>
+                                원하는 중고책? <br/> Herb Book Store!
+                            </Typography>
+                            <img
+                                src={bookImage}
+                                alt={'책장에 책이 가득한 사진 - Photo by freddie marriage on Unsplash'}
+                            />
+                        </SectionStyle>
+                    </MHidden>
+                </Grid>
+                <Grid item md={9} xs={12}>
+                    <Container maxWidth="sm">
+                        <LoginPage/>
+                    </Container>
+                </Grid>
+            </Grid>
+        </RootStyle>
+    );
 }
