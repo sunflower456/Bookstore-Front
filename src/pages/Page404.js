5a450543d97777684a95dc8b02da707c3844d709
gentledot <gentledot.wp@gmail.com>
2021-07-26 20:07:20 +0900

    feat : 404 page 생성 (2)

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -1,33 +1,56 @@
 import {Link as RouterLink} from 'react-router-dom';
 // material
 import {styled} from '@material-ui/core/styles';
-import {Box, Button, Container} from '@material-ui/core';
+import {Box, Button, Container, Paper, Typography} from '@material-ui/core';
+import {makeStyles} from '@material-ui/styles';
 // components
 import Page from '../components/Page';
+// images
+import logo from '../static/images/herbLogo.png'
 
 // ----------------------------------------------------------------------
 
-const RootStyle = styled(Page)(({ theme }) => ({
-  display: 'flex',
-  minHeight: '100%',
-  alignItems: 'center',
-  paddingTop: theme.spacing(15),
-  paddingBottom: theme.spacing(10)
+const RootStyle = styled(Page)(({theme}) => ({
+    display: 'flex',
+    minHeight: '100%',
+    alignItems: 'center',
+    paddingTop: theme.spacing(15),
+    paddingBottom: theme.spacing(10)
 }));
+
+const pageStyle = makeStyles(theme => ({
+        errorPaper: {
+            padding: theme.spacing(2)
+        },
+        backButton:{
+            marginTop : theme.spacing(3),
+        }
+    })
+);
 
 // ----------------------------------------------------------------------
 
 export default function Page404() {
-  return (
-    <RootStyle title="404 Page Not Found | Minimal-UI">
-      <Container>
-          <Box sx={{ maxWidth: 480, margin: 'auto', textAlign: 'center' }}>
-            404 not found!
-            <Button to="/" size="large" variant="contained" component={RouterLink}>
-              Go to Home
-            </Button>
-          </Box>
-      </Container>
-    </RootStyle>
-  );
+    const classes = pageStyle();
+
+    return (
+        <RootStyle title="Page Not Found | Herb Book Store">
+            <Container maxWidth='xs' fixed>
+                <Paper className={classes.errorPaper} elevation={3}>
+                    <Typography
+                        variant="h1"
+                        color="secondary"
+                    >
+                        404
+                    </Typography>
+                    <Typography variant="h5" color="primary">
+                        요청한 페이지를 확인할 수 없습니다.
+                    </Typography>
+                    <Button className={classes.backButton} to="/" size="large" variant="contained" component={RouterLink}>
+                        홈화면으로
+                    </Button>
+                </Paper>
+            </Container>
+        </RootStyle>
+    );
 }
