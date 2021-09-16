20e484d92b6eeb9b369039236a4648dd89a99fdf
gentledot <gentledot.wp@gmail.com>
2021-09-17 01:33:38 +0900

    refactor : 내 관심목록, 내 판매글 grid container 속성 누락 수정

--- src/components/mySales/MySaleList.js
+++ src/components/mySales/MySaleList.js
@@ -3,7 +3,7 @@ import EnhancedTable from "./table/EnhancedTable";
 
 export default function MySaleList() {
     return (
-        <Grid direction={"column"}>
+        <Grid container direction={"column"}>
             <Grid item md={6} xl={12}>
                 <Paper>
                     <Typography variant={"h3"}>내 판매글</Typography>
